4e75f85b56a38a19c2e6b7dadad00cf72a64a06f
Dave Smith <dave@sophoservices.com>
2020-11-02 15:50:07 -0600

    Added in a Domo function
    needed to add async
    renamed Cargo.toml project to rust_domo_uploader

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use serde::Deserialize;
+use domo::public::Client;
 use std::fs;
+use futures::executor::block_on;
 
 #[derive(Debug,Deserialize)]
 struct DomoConfigFile {
+    host: String,
     client_id: String,
     secret: String,
 }
@@ -20,19 +23,26 @@ struct ConfigFile {
 fn main() {
     let config = read_config();
     println!("{:#?}", config);
-    
+    let domo = Client::new(&config.domo.host, &config.domo.client_id, &config.domo.secret);
+    block_on(list_datasets(domo));
+}
+
+async fn list_datasets(domo: Client) {
+    let r = domo.get_datasets(None,None).await.unwrap();
+    println!("{:#?}", r);    
 }
 
 fn read_config() -> ConfigFile {
     let contents = match fs::read_to_string("config.toml") {
 	Ok(x) => x,
-	Err(e) => panic!("Please create config file 'config.toml' with values for domo: client_id and secret, and activecampaign: namespace and token. \n\nError: {}", e)
+	Err(e) => panic!("Please create config file 'config.toml' with values for domo: host, client_id, and secret, and activecampaign: namespace and token. \n\nError: {}", e)
     };
     return match toml::from_str(&contents) {
 	Ok(x) => x,
 	Err(e) => panic!(
 	    r#"Contents of config.toml should be 
 [domo]
+host="..."
 client_id="..."
 secret="..."
 [activecampaign]
